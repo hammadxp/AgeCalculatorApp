49d19ddfaa0bd7d356ca6d1389118d53f34a260b
hammadxp <hammadjaved9860@gmail.com>
2023-05-19 12:10:13 +0500

    added submit button as an image, why so difficult

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,14 @@
-const daySelector = document.getElementById("daySelector");
+const daySelector = document.getElementById("day-selector");
+const ageForm = document.getElementById("age-form");
 
-for (let i = 1; i <= 31; i++) {
-  let option = document.createElement("option");
-  option.value = i;
-  option.innerHTML = i;
-  daySelector.appendChild(option);
-}
+ageForm.addEventListener("submit", function () {
+  const ageData = new FormData(ageForm);
+  console.log(ageData);
+});
+
+// for (let i = 1; i <= 31; i++) {
+//   let option = document.createElement("option");
+//   option.value = i;
+//   option.innerHTML = i;
+//   daySelector.appendChild(option);
+// }
